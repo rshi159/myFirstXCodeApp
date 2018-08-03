dbe8f139c43a1c31ce6f1e9c8079940f93d0a643
rshi159 <rshi0611@gmail.com>
2018-08-03 15:21:05 -0700

    first commit

--- myFirstXCodeApp/ViewController.swift
+++ myFirstXCodeApp/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var countPress = 0
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        countPress = countPress + 1
+        if countPress >= 5 {
+            view.backgroundColor = UIColor.red
+            myLabel.text = "im cool"
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
